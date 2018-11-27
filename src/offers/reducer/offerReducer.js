2e2472db46f69bd0224fab03b29dcdc3f21c50de
William Garneau <willi_garn@hotmail.com>
2018-11-26 21:50:31 -0500

    Added offer reducer cases:

--- src/offers/reducer/offerReducer.js
+++ src/offers/reducer/offerReducer.js
@@ -1,6 +1,10 @@
 // @flow
 
-// import actions
+import {
+  FETCH_OFFERS_REQUEST,
+  FETCH_OFFERS_SUCCESS,
+  FETCH_OFFERS_FAILURE,
+} from '../actions/offersActions';
 
 const initialState = {
   fetching: false,
@@ -10,7 +14,15 @@ const initialState = {
 
 export default function reducer(state: any = initialState, action: any) {
   switch (action.type) {
-
+    case FETCH_OFFERS_REQUEST: {
+      return { ...state, fetching: true, fetched: false };
+    }
+    case FETCH_OFFERS_SUCCESS: {
+      return { ...state, fetching: false, fetched: true, offers: action.offers }; 
+    }
+    case FETCH_OFFERS_FAILURE: {
+      return { ...state, fetching: false, fetched: false };
+    }
     default:
       return state;
   }
--- src/offers/services/OfferAPI.js
+++ src/offers/services/OfferAPI.js
@@ -1,11 +1,15 @@
 // @flow
 
-import Profile from '../../authentication/domain/Profile';
+import Offer from '../domain/Offer';
 
 export async function FetchOffers(): Promise<any> {
+  let attempt
   await fetch("http://70.48.63.175:8080/JobineDB/webresources/entities.offer")
     .then((resp) => resp.json()) // Transform the data into json
     .then(function(data) {
       console.log(data);
   });
+  attempt.length > 0 ? attempt = Offer.parse(attempt[0]) : attempt = null;
+  console.log(attempt);
+  return attempt;
 }
