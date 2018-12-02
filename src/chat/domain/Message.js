bc2e10616f78aa45b4ab3d4b4d538a2bd9dd1e98
William Garneau <willi_garn@hotmail.com>
2018-12-02 17:39:42 -0500

    Removed useless comment

--- src/chat/domain/Message.js
+++ src/chat/domain/Message.js
@@ -1,11 +1,5 @@
 import { Record } from 'immutable';
 
-/*contentMsg: "salut xd je m'appel bob, même si jsuis mécanicien d'engin lourd j'aimerais bien travailler chex nexapp! "
-dateMsg: "2018-11-30T05:00:00Z[UTC]"
-idMsg: 1
-idOffer: 1
-idUser: 1*/
-
 const OfferRecord = Record({
   contentMsg: undefined,
   dateMsg: undefined,
